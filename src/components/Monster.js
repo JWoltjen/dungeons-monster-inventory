ee4caf7e3b6c92b8116558f48e60c17485477fa4
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-06-01 11:14:02 -0500

    create monster and monster list component shells

---
+++ src/components/Monster.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Monster() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Monster
---
+++ src/components/MonsterList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MonsterList() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default MonsterList
