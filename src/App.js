c1e5ceeae492caf29171a8fb7b3127027e3e69ff
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-06-01 11:15:38 -0500

    clear vestigial code from css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import {BrowserRouter, Route, Link} from 'react-router-dom'; 
 import './App.css';
+import Nav from './components/Nav'
 
 function App() {
   return (
     <div className="App">
-      
+      <Nav /> 
     </div>
   );
 }
