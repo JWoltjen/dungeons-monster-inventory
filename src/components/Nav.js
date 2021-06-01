a13b34eccad9f24ecfd355b08bb0679d0d673d42
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-06-01 11:20:27 -0500

    add navbar links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,9 +2,19 @@ import React from 'react'
 
 function Nav() {
     return (
-        <div className='nav'>
-            
-        </div>
+        <nav>
+            <h3>
+                Logo
+            </h3>
+            <ul>
+                <li>
+                    Saved Monsters
+                </li>
+                <li>
+                    Monster Kill Count
+                </li>
+            </ul>
+        </nav>
     )
 }
 
