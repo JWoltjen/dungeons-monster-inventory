8cb6b2fc15dbb3e8826859e69d23daeeaac18c57
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-06-01 13:42:34 -0500

    add links to savedmonsters and killedmonsters components

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'; 
 
 function Nav() {
     return (
@@ -7,9 +8,16 @@ function Nav() {
                 Logo
             </h3>
             <ul className="nav-links">
-                <li>
-                    Saved Monsters
-                </li>
+                <Link to='/savedmonsters'>
+                    <li>
+                        Saved Monsters
+                    </li>
+                </Link>
+                <Link to='/killedmonsters'>
+                    <li>
+                        Killed Monsters
+                    </li>
+                </Link>
                 <li>
                     Monster Kill Count
                 </li>
