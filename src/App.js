0b444486c97192f8a4a3d8b88c91dbbfade9d756
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-06-01 11:32:05 -0500

    add simple search shell to app.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import {BrowserRouter, Route, Link} from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav'
 import MonsterList from './components/MonsterList'
+
+
 function App() {
   return (
     <div className="App">
       <Nav /> 
+      <form className='search-form'>
+        <input className='search-bar' placeholder='input monster' type="text" />
+        <button className='search-button' type='submit'>
+          Search
+        </button>
+      </form>
       <MonsterList />
     </div>
   );
