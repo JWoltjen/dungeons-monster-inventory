7bfdb98346a7840d18da019405ccbfa23862dfef
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-06-01 13:51:50 -0500

    hit api to return array of all monster objects

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import React, {useState, useEffect} from 'react'
 import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'; 
 import './App.css';
 import Nav from './components/Nav'
 import MonsterList from './components/MonsterList'
 import SavedMonsters from './components/SavedMonsters'
 import KilledMonsters from './components/KilledMonsters'
+import axios from 'axios';
+
+
 
 function App() {
+
+  useEffect(() => {
+    getMonsters()
+  }, [])
+
+  const getMonsters = async () => {
+    const response = await fetch(`https://www.dnd5eapi.co/api/monsters`)
+    const data = await response.json(); 
+    console.log(data)
+  }
   return (
     <Router>
       <div className="App">
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,23 +2,26 @@ import React from 'react'
 import {Link} from 'react-router-dom'; 
 
 function Nav() {
+    const navStyle = {
+        color: 'white'
+    }
     return (
         <nav>
             <h3>
                 Logo
             </h3>
             <ul className="nav-links">
-                <Link to='/savedmonsters'>
+                <Link style={navStyle} to='/savedmonsters'>
                     <li>
                         Saved Monsters
                     </li>
                 </Link>
-                <Link to='/killedmonsters'>
+                <Link style={navStyle} to='/killedmonsters'>
                     <li>
                         Killed Monsters
                     </li>
                 </Link>
-                <li>
+                <li style={navStyle}>
                     Monster Kill Count
                 </li>
             </ul>
